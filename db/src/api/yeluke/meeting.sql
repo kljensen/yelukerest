bdb02a3a36d77bfb136ed5e21e0737e0dadcda08
Kyle L. Jensen <kljensen@gmail.com>
2017-12-27 16:00:07 -0500

    Formatting

--- db/src/api/yeluke/meeting.sql
+++ db/src/api/yeluke/meeting.sql
@@ -1,3 +1,5 @@
 create or replace view meetings as
-select * from data.meeting;
-alter view meetings owner to api; -- it is important to set the correct owner to the RLS policy kicks in
+    select * from data.meeting;
+
+-- it is important to set the correct owner so the RLS policy kicks in
+alter view meetings owner to api;
