6156080a6be1e70ea646fb3d7d5b986aca549174
Kyle L. Jensen <kljensen@gmail.com>
2017-12-27 15:11:53 -0500

    meetings view returns all columns

--- db/src/api/yeluke/meeting.sql
+++ db/src/api/yeluke/meeting.sql
@@ -1,3 +1,3 @@
 create or replace view meetings as
-select id from data.meeting;
+select * from data.meeting;
 alter view meetings owner to api; -- it is important to set the correct owner to the RLS policy kicks in
