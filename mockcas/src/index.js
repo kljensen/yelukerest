3c2c34b37c51367568fc8bdc2e068e4c26d8447e
Kyle L. Jensen <kljensen@gmail.com>
2018-01-02 09:57:34 -0500

    Add better logging

--- mockcas/src/index.js
+++ mockcas/src/index.js
@@ -1,6 +1,32 @@
 // This is a mock CAS server. We use it for testing only.
 const app = require('express')();
 const path = require('path');
+const morgan = require('morgan');
+const winston = require('winston');
+
+// Set up logging
+const level = process.env.LOG_LEVEL || 'debug';
+const logger = new winston.Logger({
+    transports: [
+        new winston.transports.Console({
+            level: level,
+            timestamp: function () {
+                return (new Date())
+                    .toISOString();
+            }
+        })
+    ]
+});
+
+// Tell morgan to log through this winston logger
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms', {
+    stream: {
+        write: msg => {
+            logger.info('-----------------');
+            logger.info(msg);
+        }
+    }
+}));
 
 // Set up our view engine
 app.engine('pug', require('pug')
@@ -9,11 +35,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/cas/serviceValidate', (req, res) => {
-    console.log('Got request to /cas/serviceValidate');
     res.set('Content-Type', 'text/xml');
     const failureContent = '<cas:serviceResponse xmlns:cas="https://www.yale.edu/tp/cas"><cas:authenticationFailure code="INVALID_TICKET">Ticket ST-1856339-aA5Yuvrxzpv8Tau1cYQ7 not recognized</cas:authenticationFailure></cas:serviceResponse>';
     const ticket = req.query.ticket;
     if (!ticket) {
+        logger.info('\t* No ticket in query parameters. Sending INVALID_TICKET response.');
         return res.send(failureContent);
     }
 
@@ -21,17 +47,19 @@ app.get('/cas/serviceValidate', (req, res) => {
     // where blah234 is the netid of the person authenticating.
     const ticketBits = ticket.split('-');
     if (ticketBits.length != 3 || ticketBits[0] != 'mock' || ticketBits[1] != 'ticket') {
+        logger.info('\t* Ticket not in expected format. Sending INVALID_TICKET response.');
         return res.send(failureContent);
     }
     const netid = ticketBits[2];
     const response = `<cas:serviceResponse xmlns:cas="https:/www.yale.edu/tp.cas"><cas:authenticationSuccess><cas:user>${netid}</cas:user><cas:foo>bar</cas:foo></cas:authenticationSuccess></cas:serviceResponse>`;
+    logger.info(`\t* No ticket in query parameters. Sending authentication success response for netid "${netid}".`);
     return res.send(response);
 });
 
 app.get('/cas/login', (req, res) => {
-    console.log('Got request to /cas/login');
     const service = req.query.service;
     if (!service) {
+        logger.info('\t* No service specified in query. Sending 400 response');
         res.status(400);
         res.end();
         return;
@@ -39,9 +67,11 @@ app.get('/cas/login', (req, res) => {
     // If they supplied a userid, just do the redirect straight off
     const id = req.query.id;
     if (id) {
+        logger.info(`\t* Received netid "${id}" in query. Redirecting to service ${service}`);
         res.redirect(service + `?ticket=mock-ticket-${id}`);
         return;
     }
+    logger.info(`\t* Displaying login form for service ${service}`);
     res.render('login', {
         title: 'MockCAS',
         message: 'Fill in the netid with with you want to authenticate!',
@@ -57,5 +87,5 @@ app.get('/cas/login', (req, res) => {
 const port = process.env.PORT || 8000;
 const host = process.env.HOST || '0.0.0.0';
 app.listen(port, host, () => {
-    console.log(`MockCAS running on ${host}:${port}`);
+    logger.info(`MockCAS running on ${host}:${port}`);
 });
