cc1699593e53d76be4b56246b6476a122748e2bc
Kyle L. Jensen <kljensen@gmail.com>
2018-01-02 12:45:21 -0500

    Add auth tests

--- tests/rest/yeluke/auth.js
+++ tests/rest/yeluke/auth.js
@@ -5,6 +5,8 @@ const resetdb = common.resetdb;
 const baseURL = common.baseURL;
 const request = require('supertest');
 const should = require("should");
+const chai = require('chai');
+const url = require('url');
 
 // Most of what we're testing here requires authentication.
 // We're going to need to set-up a mock CAS server,
@@ -12,20 +14,38 @@ const should = require("should");
 // The, we'll need to dynamically get JWT tokens from our
 // app. Also, we'll need to store credentials after logging-in. 
 // See https://medium.com/@juha.a.hytonen/testing-authenticated-requests-with-supertest-325ccf47c2bb
+// and https://github.com/visionmedia/superagent/blob/master/test/node/agency.js
 
-describe('Yeluke CAS auth', function () {
+const authPath = '/auth/login';
+
+describe('Yeluke auth using CAS', function () {
     before(function (done) {
         resetdb();
         done();
     });
 
     it('login page should give a temporary redirect to the CAS server', async() => {
-        console.error('woot');
-        const response = await request(`${baseURL}`)
-            .get('/auth/login')
+        const response = await request(baseURL)
+            .get(authPath)
             .expect('Location', /http/)
             .expect(307);
-        console.error('done with response');
     });
+
+    it('should redirect back to the originating service after entering credentials', async() => {
+        const response1 = await request(baseURL)
+            .get(authPath)
+            .redirects(1);
+        chai.expect(response1.redirects).to.have.lengthOf(1);
+        const casURL = new url.URL(response1.redirects[0]);
+        // console.error(`cas url = ${casURL}`);
+        const agent = request.agent(casURL.host);
+        const netid = 'klj39';
+        const path = `${casURL.pathname}${casURL.search}&id=${netid}`;
+        // console.error(path);
+        await agent
+            .get(path) // post to the current URL
+            .expect(302);
+    });
+
 
 });
