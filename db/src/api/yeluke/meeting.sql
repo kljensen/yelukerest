c9b53755dbfde1e19a80fb21d712f6e35edfd5c5
Kyle L. Jensen <kljensen@gmail.com>
2018-01-02 06:53:22 -0500

    Add comments on the meetings view so it shows up in swagger ui

--- db/src/api/yeluke/meeting.sql
+++ db/src/api/yeluke/meeting.sql
@@ -3,3 +3,24 @@ create or replace view meetings as
 
 -- it is important to set the correct owner so the RLS policy kicks in
 alter view meetings owner to api;
+
+COMMENT ON VIEW meetings IS
+    'An in-person meeting of our class, usually a lecture';
+COMMENT ON COLUMN meetings.id IS
+    'A surrogate primary key';
+COMMENT ON COLUMN meetings.slug IS
+    'A short identifier, appropriate for URLs, like "sql-intro"';
+COMMENT ON COLUMN meetings.summary IS
+    'A short description of the meeting in Markdown format';
+COMMENT ON COLUMN meetings.description IS
+    'A long description of the meeting in Markdown format';
+COMMENT ON COLUMN meetings.begins_at IS
+    'The time at which the meeting begins, including timezone';
+COMMENT ON COLUMN meetings.duration IS
+    'The duration of the meeting as a Postgres interval';
+COMMENT ON COLUMN meetings.is_draft IS
+    'An indicator of if the content is still changing';
+COMMENT ON COLUMN meetings.created_at IS
+    'The time this database entry was created, including timezone';
+COMMENT ON COLUMN meetings.updated_at IS
+    'The most recent time this database entry was updated, including timezone';
