96d564781cc6af8289e6242d0bddc01934ba4365
Kyle L. Jensen <kljensen@gmail.com>
2018-01-04 13:28:28 -0500

    Use polling for nodemon

--- authapp/run.sh
+++ authapp/run.sh
@@ -2,4 +2,4 @@
 set -e
 # Watch the `src` directory for changes to files ending
 # in 'js' and restart the `src/server.js` process.
-./node_modules/.bin/nodemon -e js -w src -V src/server.js
+./node_modules/.bin/nodemon -L -e js -w src -V src/server.js
