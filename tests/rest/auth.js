44d1f0b482eee657607cc2dd398eb1d4f6300230
Kyle L. Jensen <kljensen@gmail.com>
2018-01-04 12:07:41 -0500

    Eslint

--- tests/rest/auth.js
+++ tests/rest/auth.js
@@ -7,70 +7,70 @@ const request = require('supertest');
 const should = require("should");
 
 describe.skip('auth', function () {
-  before(function (done) {
-    resetdb();
-    done();
-  });
+    before(function (done) {
+        resetdb();
+        done();
+    });
 
-  it('login', function (done) {
-    restService()
-      .post('/rpc/login?select=me,token')
-      .set('Accept', 'application/vnd.pgrst.object+json')
-      .send({
-        email: "alice@email.com",
-        password: "pass"
-      })
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-      .expect(r => {
-        //console.log(r.body)
-        r.body.me.email.should.equal('alice@email.com');
-      })
-  });
+    it('login', function (done) {
+        restService()
+            .post('/rpc/login?select=me,token')
+            .set('Accept', 'application/vnd.pgrst.object+json')
+            .send({
+                email: "alice@email.com",
+                password: "pass"
+            })
+            .expect('Content-Type', /json/)
+            .expect(200, done)
+            .expect(r => {
+                //console.log(r.body)
+                r.body.me.email.should.equal('alice@email.com');
+            })
+    });
 
-  it('me', function (done) {
-    restService()
-      .post('/rpc/me')
-      .set('Accept', 'application/vnd.pgrst.object+json')
-      .set('Authorization', 'Bearer ' + jwt)
-      .send({})
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-      .expect(r => {
-        //console.log(r.body)
-        r.body.email.should.equal('alice@email.com');
-      })
-  });
+    it('me', function (done) {
+        restService()
+            .post('/rpc/me')
+            .set('Accept', 'application/vnd.pgrst.object+json')
+            .set('Authorization', 'Bearer ' + jwt)
+            .send({})
+            .expect('Content-Type', /json/)
+            .expect(200, done)
+            .expect(r => {
+                //console.log(r.body)
+                r.body.email.should.equal('alice@email.com');
+            })
+    });
 
-  it('refresh_token', function (done) {
-    restService()
-      .post('/rpc/refresh_token')
-      .set('Accept', 'application/vnd.pgrst.object+json')
-      .set('Authorization', 'Bearer ' + jwt)
-      .send({})
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-      .expect(r => {
-        //console.log(r.body)
-        r.body.length.should.above(0);
-      })
-  });
+    it('refresh_token', function (done) {
+        restService()
+            .post('/rpc/refresh_token')
+            .set('Accept', 'application/vnd.pgrst.object+json')
+            .set('Authorization', 'Bearer ' + jwt)
+            .send({})
+            .expect('Content-Type', /json/)
+            .expect(200, done)
+            .expect(r => {
+                //console.log(r.body)
+                r.body.length.should.above(0);
+            })
+    });
 
-  it('signup', function (done) {
-    restService()
-      .post('/rpc/signup')
-      .set('Accept', 'application/vnd.pgrst.object+json')
-      .send({
-        name: "John Doe",
-        email: "john@email.com",
-        password: "pass"
-      })
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-      .expect(r => {
-        //console.log(r.body)
-        r.body.me.email.should.equal('john@email.com');
-      })
-  });
+    it('signup', function (done) {
+        restService()
+            .post('/rpc/signup')
+            .set('Accept', 'application/vnd.pgrst.object+json')
+            .send({
+                name: "John Doe",
+                email: "john@email.com",
+                password: "pass"
+            })
+            .expect('Content-Type', /json/)
+            .expect(200, done)
+            .expect(r => {
+                //console.log(r.body)
+                r.body.me.email.should.equal('john@email.com');
+            })
+    });
 
 });
--- tests/rest/read.js
+++ tests/rest/read.js
@@ -7,30 +7,30 @@ const request = require('supertest');
 const should = require("should");
 
 describe('read', function () {
-  before(function (done) {
-    resetdb();
-    done();
-  });
+    before(function (done) {
+        resetdb();
+        done();
+    });
 
-  it('basic', function (done) {
-    restService()
-      .get('/todos?select=id,todo')
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-      .expect(r => {
-        r.body.length.should.equal(3);
-        r.body[0].id.should.equal(1);
-      })
-  });
+    it('basic', function (done) {
+        restService()
+            .get('/todos?select=id,todo')
+            .expect('Content-Type', /json/)
+            .expect(200, done)
+            .expect(r => {
+                r.body.length.should.equal(3);
+                r.body[0].id.should.equal(1);
+            })
+    });
 
-  it('by primary key', function (done) {
-    restService()
-      .get('/todos/1?select=id,todo')
-      .expect(200, done)
-      .expect(r => {
-        r.body.id.should.equal(1);
-        r.body.todo.should.equal('item_1');
-      })
-  });
+    it('by primary key', function (done) {
+        restService()
+            .get('/todos/1?select=id,todo')
+            .expect(200, done)
+            .expect(r => {
+                r.body.id.should.equal(1);
+                r.body.todo.should.equal('item_1');
+            })
+    });
 
 });
