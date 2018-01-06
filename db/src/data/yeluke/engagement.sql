706461b2ca364a4194406aa0cc26463829e38a1d
Kyle L. Jensen <kljensen@gmail.com>
2018-01-06 07:45:05 -0500

    Update engagement timestamps with trigger

--- db/src/data/yeluke/engagement.sql
+++ db/src/data/yeluke/engagement.sql
@@ -4,6 +4,11 @@ CREATE TABLE IF NOT EXISTS engagement (
     user_id INT REFERENCES "user"(id) ON DELETE CASCADE,
     meeting_id INT REFERENCES meeting(id) ON DELETE CASCADE, 
     participation participation_enum NOT NULL,
+    -- We are not going to have a constraint that the 
+    -- `created_at` be after the `meeting_id.begins_at` because
+    -- some students will have an excused absence in advance
+    -- of a class and we want to record those students as
+    -- present.
     created_at TIMESTAMP WITH TIME ZONE
         NOT NULL
         DEFAULT current_timestamp,
@@ -12,4 +17,8 @@ CREATE TABLE IF NOT EXISTS engagement (
         DEFAULT current_timestamp,
     CONSTRAINT updated_after_created CHECK (updated_at >= created_at),
     PRIMARY KEY(user_id, meeting_id)
-);
+);CREATE TRIGGER tg_engagement_update_timestamps
+    BEFORE INSERT OR UPDATE
+    ON engagement
+    FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
