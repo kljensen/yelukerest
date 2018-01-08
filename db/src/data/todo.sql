b2ebf1f5f2f9ac8a2d0018453b3c8eee7317cbcb
Kyle L. Jensen <kljensen@gmail.com>
2018-01-08 11:48:02 -0500

    Add cascade actions on owner_id fk

--- db/src/data/todo.sql
+++ db/src/data/todo.sql
@@ -2,7 +2,9 @@ create table todo (
 	id    serial primary key,
 	todo  text not null,
 	private boolean default true,  
-	owner_id int references "user"(id) default request.user_id()
+	owner_id int references "user"(id)
+		ON UPDATE CASCADE ON DELETE CASCADE
+		default request.user_id()
 );
 
 create trigger send_change_event
