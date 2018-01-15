1985bb2422102d61e7893a80a119590f62af72d2
Kyle L. Jensen <kljensen@gmail.com>
2018-01-15 12:31:54 -0500

    Add command to add a user

--- pythonclient/db_client.py
+++ pythonclient/db_client.py
@@ -4,22 +4,38 @@
     bulk administration connecting directly to the database.
 """
 import click
+import os
 import psycopg2
+
 
 @click.group()
 @click.pass_context
-@click.argument('dburl', envvar="DATABASE_URL")
-def db(ctx, dburl):
+def database(ctx):
     """ A function that groups together various commands and
         passes along a database connection in the context.
     """
+    try:
+        dburl = os.environ['DATABASE_URL']
+    except KeyError:
+        raise Exception("you must provide a DATABASE_URL in the environment")
     conn = psycopg2.connect(dburl)
     ctx.obj['conn'] = conn
 
-@db.command()
-def sayhi():
-    click.echo("hi")
+
+@database.command()
+@click.pass_context
+@click.argument('netid')
+@click.argument('role')
+@click.argument('nickname')
+def adduser(ctx, netid, role, nickname):
+    """ Add a user to the database
+    """
+    conn = ctx.obj['conn']
+    cur = conn.cursor()
+    statement = 'INSERT INTO data.user (netid, role, nickname) VALUES (%s, %s, %s)'
+    cur.execute(statement, (netid, role, nickname))
+    conn.commit()
 
 
 if __name__ == "__main__":
-    db(obj={})
+    database(obj={})
