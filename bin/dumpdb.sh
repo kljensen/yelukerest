1d39eb82801ce56eb47fa847a0f0f1933df84952
Kyle L. Jensen <kljensen@gmail.com>
2018-01-18 10:56:11 -0500

    Update db backup script

--- bin/dumpdb.sh
+++ bin/dumpdb.sh
@@ -3,16 +3,36 @@
 if [ -z "$1" ]
   then
     echo "You must supply a host"
+    exit
 fi
 if [ -z "$1" ]
   then
     echo "You must supply a port"
+    exit
+fi
+if [ -z "$3" ]
+  then
+    echo "You must supply an output directory"
+    exit
 fi
 
 # Dump the table
 
+PGPASSWORD=$SUPER_USER_PASSWORD pg_dumpall \
+    --host $1 --port $2 \
+     -U $SUPER_USER \
+     -g \
+    $DB_NAME >$3/globals.sql 
+
 PGPASSWORD=$SUPER_USER_PASSWORD pg_dump \
     --host $1 --port $2 \
      -U $SUPER_USER \
-     -c \
-    $DB_NAME 
+     -Fp -s -v -f $3/schema.sql \
+    $DB_NAME
+
+
+PGPASSWORD=$SUPER_USER_PASSWORD pg_dump \
+    --host $1 --port $2 \
+     -U $SUPER_USER \
+    -Fc -v -f $3/full.dump \
+    $DB_NAME
