47df6ed488ce3372879d70e18bd81dd3d24a5280
Kyle L. Jensen <kljensen@gmail.com>
2018-01-17 21:26:49 -0500

    Add command to fill in user details from ldap

--- pythonclient/db_client.py
+++ pythonclient/db_client.py
@@ -5,6 +5,7 @@
 """
 import click
 import os
+import ldap3
 import psycopg2
 from names import get_student_nickname
 
@@ -21,6 +22,58 @@ def database(ctx):
         raise Exception("you must provide a DATABASE_URL in the environment")
     conn = psycopg2.connect(dburl)
     ctx.obj['conn'] = conn
+
+
+def do_ldap_search(netid):
+    """ Search Yale LDAP for a particular netid
+    """
+    server_uri = "ldap://directory.yale.edu:389"
+    search_base = "ou=People,o=yale.edu"
+    search_filter = "(uid={0})".format(netid)
+    attributes = "*"
+    server = ldap3.Server(server_uri)
+    conn = ldap3.Connection(server, auto_bind=True)
+    conn.search(search_base, search_filter, attributes=attributes)
+    try:
+        return conn.entries[0]
+    except IndexError:
+        return None
+
+
+def ldapget(result, key):
+    try:
+        return str(getattr(result, key))
+    except ldap3.core.exceptions.LDAPKeyError:
+        return None
+
+
+@database.command()
+@click.pass_context
+def getuserldap(ctx):
+    """ Fill in info from Yale LDAP for all students
+    """
+    conn = ctx.obj['conn']
+    cur = conn.cursor()
+    statement = 'SELECT netid FROM data.user'
+    cur.execute(statement)
+    netids = set([row[0] for row in cur.fetchall()])
+    conn.commit()
+
+    for netid in netids:
+        result = do_ldap_search(netid)
+        if result:
+            known_as = ldapget(result, 'knownAs')
+            if not known_as:
+                known_as = ldapget(result, "givenName")
+            name = ldapget(result, 'cn')
+            email = ldapget(result, 'mail')
+            print(known_as)
+            print(name)
+            print(email)
+            cur = conn.cursor()
+            statement = 'UPDATE data.user SET known_as = %s, name = %s, email = %s WHERE netid = %s'
+            cur.execute(statement, (known_as, name, email, netid))
+            conn.commit()
 
 
 @database.command()
@@ -49,7 +102,6 @@ def addstudents(ctx, infile):
         Run like `honcho run python ./db_client.py adduser klj39 faculty short-owl`
     """
     users = [line.strip() for line in infile]
-    print(users)
     conn = ctx.obj['conn']
     cur = conn.cursor()
     statement = 'SELECT nickname FROM data.user'
