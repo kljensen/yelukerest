17558b401e298987d86a702731b11ac137856d45
Kyle L. Jensen <kljensen@gmail.com>
2018-08-07 18:29:26 -0400

    Do not load todo schema

--- db/src/api/schema.sql
+++ db/src/api/schema.sql
@@ -14,5 +14,5 @@ grant api to current_user; -- this is a workaround for RDS where the master user
 -- \ir ../libs/auth/api/all.sql
 
 -- our endpoints
-\ir todos.sql
+-- \ir todos.sql
 \ir yeluke.sql
