0c4905640b648b31bad0408d32c7664a84693fab
Kyle L. Jensen <kljensen@gmail.com>
2019-12-17 10:00:56 -0500

    Documentation for systemfd

--- authapp2/run.sh
+++ authapp2/run.sh
@@ -2,5 +2,8 @@
 if [[ -z "${DEVELOPMENT}" ]]; then
     echo "Running in production!"
 else
+    # Systemfd will help us move an open socket to the
+    # new process when the process restarts. cargo-watch
+    # will start a new process when our code changes.
     systemfd --no-pid -s http::0.0.0.0:${PORT} -- cargo watch -x run
 fi
