4ff4853b5e5f3e298c23d9b2e747b7c41bef304f
Kyle L. Jensen <kljensen@gmail.com>
2020-05-10 09:29:52 -0400

    Hello world with redis

--- openresty/entrypoint.sh
+++ openresty/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
-# nginx needes resolver definitions for the host name to work
-# since that (IP) is a specific setting for each deployment
-# we make it by translating the hostname on startup
+# nginx cannot use domain names to connect to upstread services but
+# instead needs IP addresses (unless you provide a resolver). Instead,
+# I'm going to overwrite each domain (host) with its IP address by looking
+# it up with getent once. Obviously then, these can't change! We do this
+# once on startup.
 DB_HOST=`getent hosts $DB_HOST | awk '{ print $1 }'`
 POSTGREST_HOST=`getent hosts $POSTGREST_HOST | awk '{ print $1 }'`
 AUTHAPP_HOST=`getent hosts $AUTHAPP_HOST | awk '{ print $1 }'`
@@ -11,6 +13,8 @@ SSE_HOST=`getent hosts $SSE_HOST | awk '{ print $1 }'`
 SHAREDTERMINAL_HOST=`getent hosts $SHAREDTERMINAL_HOST | awk '{ print $1 }'`
 ELMCLIENT_HOST=`getent hosts $ELMCLIENT_HOST | awk '{ print $1 }'`
 CERTBOT_HOST=`getent hosts $CERTBOT_HOST | awk '{ print $1 }'`
+REDIS_HOST=`getent hosts $REDIS_HOST | awk '{ print $1 }'`
 
-envsubst '$FQDN$OPENRESTY_DEVELOPMENT$OPENRESTY_CACHE_BYPASS' </usr/local/openresty/nginx/conf/nginx.conf.tmpl |tee /usr/local/openresty/nginx/conf/nginx.conf
+env_vars='$SESSION_SECRET$REDIS_HOST$REDIS_PORT$FQDN$OPENRESTY_DEVELOPMENT$OPENRESTY_CACHE_BYPASS'
+envsubst $env_vars </usr/local/openresty/nginx/conf/nginx.conf.tmpl |tee /usr/local/openresty/nginx/conf/nginx.conf
 exec /usr/local/openresty/bin/openresty -g "daemon off; error_log /dev/stderr info;"
---
+++ openresty/lualib/user_code/README.md
@@ -0,0 +1,6 @@
+This is custom lua code that will be made available in addition to the
+default code distributed with OpenResty. Some packages are vendored,
+particularly
+
+https://github.com/bungle/lua-resty-session.git
+
