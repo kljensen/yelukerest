474244ff2de707872b010ba0e172851516e93ff8
Kyle L. Jensen <kljensen@gmail.com>
2020-05-11 22:08:17 -0400

    Store tickets in shm and do redirect

--- openresty/entrypoint.sh
+++ openresty/entrypoint.sh
@@ -20,11 +20,13 @@ REDIS_HOST=`getent hosts $REDIS_HOST | awk '{ print $1 }'`
 #
 if [ -z "${DEVELOPMENT}" ]
 then
+    export NGINX_HTTP_DEV_INCLUDES=""
     export NGINX_SERVER_DEV_INCLUDES=""
 else
-    export NGINX_SERVER_DEV_INCLUDES="include includes/dev/*.conf;"
+    export NGINX_HTTP_DEV_INCLUDES="include includes/dev/http/*.conf;"
+    export NGINX_SERVER_DEV_INCLUDES="include includes/dev/http/server/*.conf;"
 fi
 
-env_vars='$NGINX_SERVER_DEV_INCLUDES$SESSION_SECRET$REDIS_HOST$REDIS_PORT$FQDN$OPENRESTY_DEVELOPMENT$OPENRESTY_CACHE_BYPASS'
+env_vars='$NGINX_HTTP_DEV_INCLUDES$NGINX_SERVER_DEV_INCLUDES$SESSION_SECRET$REDIS_HOST$REDIS_PORT$FQDN$OPENRESTY_DEVELOPMENT$OPENRESTY_CACHE_BYPASS'
 envsubst $env_vars </usr/local/openresty/nginx/conf/nginx.conf.tmpl |tee /usr/local/openresty/nginx/conf/nginx.conf
 exec /usr/local/openresty/bin/openresty -g "daemon off; error_log /dev/stderr info;"
--- openresty/lualib/user_code/mockcas.lua
+++ openresty/lualib/user_code/mockcas.lua
@@ -2,8 +2,39 @@
 local form = [[
     <html><head><title>MockCAS</title></head><body><h1>Fill in the netid with with you want to authenticate!</h1><form method="GET"><div><div><label for="id">Netid:</label></div><div><input type="text" name="id"></div></div><div><div><label for="service">Service:</label></div><div><input type="text" name="service" value="foo"></div></div><div><button type="submit">Submit</button></div></form></body></html>
 ]]
+
+local ticket_store = ngx.shared.mockcas_tickets
+local ticket_ttl = 600
+
+local function redirect_to_service(id_param, service_param)
+    local ticket = "mock-ticket-" .. id_param
+    local succ, err, _ = ticket_store:set(ticket, id_param, ticket_ttl)
+    if succ ~= true then
+        return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    end
+    local encoded_args =  ngx.encode_args({ticket = ticket})
+    local redirect_url = service_param .. "?" .. encoded_args
+    return ngx.redirect(redirect_url, ngx.HTTP_MOVED_TEMPORARILY)
+end
+
 local function cas_login_form()
-    return ngx.say(form)
+
+    -- See if we have `id` or `service` parameters
+    -- in the request URI.
+    local args, err = ngx.req.get_uri_args()
+    id_param = args["id"]
+    service_param = args["service"]
+
+    -- Either show them a form or redirect.
+    if id_param == nil or service_param == nil then
+        -- Give the user a fake login page.
+        return ngx.say(form)
+    else
+        -- We have a user id and a service,
+        -- create a mock ticket and redirect
+        -- the user back to the service.
+        return redirect_to_service(id_param, service_param)
+    end
 end
 
 return {
