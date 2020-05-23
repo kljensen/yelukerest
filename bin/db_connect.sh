c06717832dcc9fea42045212a32af89a5102cb68
Kyle L. Jensen <kljensen@gmail.com>
2020-05-23 07:20:42 -0400

    Use localhost psql if possible when connecting to database

--- bin/db_connect.sh
+++ bin/db_connect.sh
@@ -6,5 +6,15 @@ set -a
 . ./.env
 set +a
 
-# Dump the table
-PGPASSWORD=$SUPER_USER_PASSWORD docker exec -it "${COMPOSE_PROJECT_NAME}_db_1" psql -U $SUPER_USER  $DB_NAME
+# See if we have psql installed locally
+if [ -x "$(command -v psql)" ]; then
+    # Connect through our host system psql (thus loading
+    # the user's .inputrc and other niceities)
+    PGPASSWORD=$SUPER_USER_PASSWORD \
+        psql --host localhost --port $DB_PORT -U $SUPER_USER  $DB_NAME 
+else
+    # Connect through psql in the docker container
+    PGPASSWORD=$SUPER_USER_PASSWORD \
+        docker exec -it "${COMPOSE_PROJECT_NAME}_db_1" \
+        psql -U $SUPER_USER  $DB_NAME 
+fi
