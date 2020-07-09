4dcde65829a1d5b8a755c24718e8ada1297eef90
Kyle L. Jensen <kljensen@gmail.com>
2020-07-09 15:43:48 -0400

    Add comment field to assignment grades. Closes #86

--- db/src/data/yeluke/assignment_grade.sql
+++ db/src/data/yeluke/assignment_grade.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS assignment_grade (
     points_possible smallint NOT NULL,
     assignment_submission_id INT PRIMARY KEY,
     points REAL NOT NULL,
+    comments TEXT,
     created_at TIMESTAMP WITH TIME ZONE
         NOT NULL
         DEFAULT current_timestamp,
--- db/src/sample_data/yeluke/assignment_grade.sql
+++ db/src/sample_data/yeluke/assignment_grade.sql
@@ -3,11 +3,11 @@
 -- Users 1 & 2 took the first assignment. Nobody took anything
 -- else. Let's make user #1 get a 100% and user #2 get a 50%.
 -- assignment #1 has two questions.
-COPY data.assignment_grade (assignment_submission_id,assignment_slug,points) FROM STDIN (ENCODING 'utf-8', FREEZE ON);
-1	team-selection	50
-2	team-selection	40
-3	team-selection	20
-4	project-update-1	75
+COPY data.assignment_grade (assignment_submission_id,assignment_slug,points,comments) FROM STDIN (ENCODING 'utf-8', FREEZE ON);
+1	team-selection	50	Foo bar bax boo this is your comment
+2	team-selection	40	Grade comment
+3	team-selection	20	
+4	project-update-1	75	\N
 \.
 
 ANALYZE data.assignment_grade;
