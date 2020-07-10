f1f13968d408e11d8e14fb7d455edcf38db2d1ad
Kyle L. Jensen <kljensen@gmail.com>
2020-07-10 14:51:58 -0400

    Add comments fields to quiz grade. Closes #120

--- db/src/data/yeluke/quiz_grade.sql
+++ db/src/data/yeluke/quiz_grade.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS quiz_grade (
     quiz_id INT NOT NULL,
     points REAL NOT NULL,
     points_possible smallint NOT NULL,
+    comments TEXT,
     user_id INT REFERENCES "user"(id)
         ON UPDATE CASCADE NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE
