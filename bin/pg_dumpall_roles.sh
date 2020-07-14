f0caa690aa3cdf696808b035de4d33d1179d5d89
Kyle L. Jensen <kljensen@gmail.com>
2020-07-14 11:22:32 -0400

    Add shell script wrapper for pg_dumpall -r

---
+++ bin/pg_dumpall_roles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script will dump all the roles needed on our pg system/cluster. Roles are
+# global and NOT specific to the database. We have various roles like 'ta', 'faculty',
+# and 'authenticator'. The database permissions will depend on these roles, e.g.
+# the `api` views are owned by the `api` role. 
+
+# Source environment variables
+set -a
+. ./.env
+set +a
+
+# Dump the table
+PGPASSWORD=$SUPER_USER_PASSWORD pg_dumpall \
+    --host $DB_DEV_HOST --port $DB_PORT \
+     -U $SUPER_USER \
+     -r
