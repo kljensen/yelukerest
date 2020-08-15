8a7af4acae87301df8d2347ff850eb1ce199ae36
Kyle L. Jensen <kljensen@gmail.com>
2020-08-15 13:31:23 -0400

    Fix jwt_secret insertion

--- bin/create-initial-migrations.sh
+++ bin/create-initial-migrations.sh
@@ -131,7 +131,7 @@ INSERT INTO secrets (key, value) VALUES ('jwt_lifetime','3600');
 INSERT INTO secrets (key, value) VALUES ('auth.default-role','anonymous');
 INSERT INTO secrets (key, value) VALUES ('auth.data-schema','data');
 INSERT INTO secrets (key, value) VALUES ('auth.api-schema','api');
-INSERT INTO secrets (key, value) VALUES ('auth.api-schema',:'jwt_secret');
+INSERT INTO secrets (key, value) VALUES ('jwt_secret',:'jwt_secret');
 
 COMMIT;
 EOF
