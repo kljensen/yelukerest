bb00f962d7c9653e3606b10116eb90ddc802ec28
Kyle L. Jensen <kljensen@gmail.com>
2020-10-28 10:57:31 -0400

    Harden the homebrew openresty waf

--- openresty/lualib/user_code/waf.lua
+++ openresty/lualib/user_code/waf.lua
@@ -1,20 +1,13 @@
--- This is a simple web application firewall that
--- uses honeypot URL patterns in order to identify
--- malicious IP addresses and block them. In this
--- script we're just checking to see if a remote
--- IP is in the list of banned IPs.
--- To use this WAF, you'd need two lines in your
--- nginx.conf:
---
--- In the http section:
--- lua_shared_dict banned_ips 2m;
---
--- and in the server or location section
--- access_by_lua_file '../lualib/user_code/waf.lua';
-local remote_addr = ngx.var.remote_addr
-local banned_ips = ngx.shared.banned_ips
-
-local found_ip, flags = banned_ips:get(remote_addr)
-if found_ip ~= nil then
-  ngx.exit(ngx.HTTP_CLOSE)
+-- Called like
+-- ban(600, ngx.HTTP_CLOSE)
+local function ban(ban_duration, response)
+    local banned_ips = ngx.shared.banned_ips
+    local remote_addr = ngx.var.remote_addr
+    local succ, err = banned_ips:set(remote_addr, 1, ban_duration)
+    ngx.log(ngx.NOTICE, "Banning " .. remote_addr)
+    return ngx.exit(response)
 end
+
+return {
+    ban = ban;
+}
