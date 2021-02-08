f88e87325bb5fc583ce5b13b043d0856cbd8e2c5
Kyle L. Jensen <kljensen@gmail.com>
2021-02-08 21:40:03 +0000

    Get certs for FQDN and NAKED_FQDN

--- bin/create-initial-certbot-https-cert.sh
+++ bin/create-initial-certbot-https-cert.sh
@@ -7,8 +7,8 @@ set +a
 
 docker volume create --name=yelukerest-letsencrypt || echo "Skipping creation of yelukerest-letsencrypt volume"
 
-if [ -n "$CERTBOT_AWS_ACCESS_KEY_ID" ] && [ -n "$CERTBOT_AWS_SECRET_ACCESS_KEY" ]; then
-    docker run -p 80:80 -it \
+get_cert_with_dns_challenge() {
+    docker run -it \
         -e AWS_ACCESS_KEY_ID=$CERTBOT_AWS_ACCESS_KEY_ID \
         -e AWS_SECRET_ACCESS_KEY=$CERTBOT_AWS_SECRET_ACCESS_KEY \
         -v yelukerest-letsencrypt:/etc/letsencrypt \
@@ -16,14 +16,30 @@ if [ -n "$CERTBOT_AWS_ACCESS_KEY_ID" ] && [ -n "$CERTBOT_AWS_SECRET_ACCESS_KEY" 
         certonly \
         --dns-route53 \
         --dns-route53-propagation-seconds 30 \
-        -d $FQDN
-else
+        -d $1
+}
+get_cert_with_http_challenge() {
     docker run -p 80:80 -it \
         -v yelukerest-letsencrypt:/etc/letsencrypt \
          certbot/dns-route53:v1.12.0 \
         certonly \
          --standalone \
          --preferred-challenges http \
-         -d $FQDN
+         -d $1
+}
+
+get_cert() {
+if [ -n "$CERTBOT_AWS_ACCESS_KEY_ID" ] && [ -n "$CERTBOT_AWS_SECRET_ACCESS_KEY" ]; then
+    get_cert_with_dns_challenge $1
+else
+    get_cert_with_http_challenge $1
 fi
+}
+
+for domain in "$NAKED_FQDN" "$FDQN" "$1"
+do
+    if [ -n "$domain" ]; then
+        get_cert $domain
+    fi
+done
 
