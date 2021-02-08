d2df5ff4838894a6a907935f2bdb7d2ef53f032f
Kyle L. Jensen <kljensen@gmail.com>
2021-02-08 12:57:03 +0000

    Add ability to use DNS/Route53 Acme challenge. Closes #147

--- bin/create-initial-certbot-https-cert.sh
+++ bin/create-initial-certbot-https-cert.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
+# vim:set tabstop=4 expandtab shiftwidth=4 smarttab
 # Source environment variables
 set -a
 . ./.env
 set +a
 
-docker run -p 80:80 -it \
-	-v yelukerest-letsencrypt:/etc/letsencrypt \
-	certbot/certbot \
-	certonly \
-	 --standalone \
-	 --preferred-challenges http \
-	 -d $FQDN
+docker volume create --name=yelukerest-letsencrypt || echo "Skipping creation of yelukerest-letsencrypt volume"
+
+if [ -n "$CERTBOT_AWS_ACCESS_KEY_ID" ] && [ -n "$CERTBOT_AWS_SECRET_ACCESS_KEY" ]; then
+    docker run -p 80:80 -it \
+        -e AWS_ACCESS_KEY_ID=$CERTBOT_AWS_ACCESS_KEY_ID \
+        -e AWS_SECRET_ACCESS_KEY=$CERTBOT_AWS_SECRET_ACCESS_KEY \
+        -v yelukerest-letsencrypt:/etc/letsencrypt \
+        certbot/dns-route53:v1.12.0 \
+        certonly \
+        --dns-route53 \
+        --dns-route53-propagation-seconds 30 \
+        -d $FQDN
+else
+    docker run -p 80:80 -it \
+        -v yelukerest-letsencrypt:/etc/letsencrypt \
+         certbot/dns-route53:v1.12.0 \
+        certonly \
+         --standalone \
+         --preferred-challenges http \
+         -d $FQDN
+fi
+
--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -2,10 +2,10 @@
 trap exit TERM;
 while true 
 do 
-	if [ -z "${DEVELOPMENT}" ]; then 
-		certbot renew
-	else 
-		echo "In development mode, not using certbot"
-	fi
-	sleep 12h
+    if [ -z "${DEVELOPMENT}" ]; then 
+        certbot renew
+    else 
+        echo "In development mode, not using certbot"
+    fi
+    sleep 12h
 done
