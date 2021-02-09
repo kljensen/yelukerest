2f2428f6e3073a6552e1487754aa07c8514c749e
Kyle L. Jensen <kljensen@gmail.com>
2021-02-09 09:45:28 -0500

    Create local cert for NAKED_FQDN and FQDN

--- bin/create-local-dev-https-cert.sh
+++ bin/create-local-dev-https-cert.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
+
+# Source environment variables
+set -a
+. ./.env
+set +a
+
 VOLUMES=yelukerest-letsencrypt:/etc/letsencrypt
 IMAGE=debian:stretch
-docker run -i -v $VOLUMES $IMAGE /bin/bash <<-EOF
-    export DEBIAN_FRONTEND=noninteractive
-    apt-get update
-    apt-get install -y openssl
-    mkdir -p /etc/letsencrypt/live/localhost
-    cd /etc/letsencrypt/live/localhost
-    openssl req -x509 -out fullchain.pem -keyout privkey.pem -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' -extensions EXT -config <( \\
-        printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+
+make_cert() {
+    domain=$1
+    printf "%s\n" "Making cert for domain $domain"
+    docker run -i -v $VOLUMES $IMAGE /bin/bash <<-EOF
+        export DEBIAN_FRONTEND=noninteractive
+        apt-get update
+        apt-get install -y openssl
+        mkdir -p /etc/letsencrypt/live/$domain
+        cd /etc/letsencrypt/live/$domain
+        openssl req -x509 -out fullchain.pem -keyout privkey.pem -newkey rsa:2048 -nodes -sha256 -subj '/CN=$domain' -extensions EXT -config <( \\
+            printf "[dn]\nCN=$domain\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:$domain\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 EOF
+}
+
+for domain in "$NAKED_FQDN" "$FDQN" "$1"
+do
+   if [ -n "$domain" ]; then
+       make_cert $domain
+   fi
+done
+
