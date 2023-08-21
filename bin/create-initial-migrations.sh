38f54244d568e8219b5c6a0f9fa038ef0e8942a5
Kyle L. Jensen <kljensen@gmail.com>
2023-08-21 17:02:34 -0400

    Raise exception if JWT secret is too short

--- bin/create-initial-migrations.sh
+++ bin/create-initial-migrations.sh
@@ -125,6 +125,15 @@ cat << EOF > $MIGRATIONS_DIRECTORY/deploy/data.sql
 BEGIN;
 \set jwt_secret \`echo \$JWT_SECRET\`
 
+
+-- Raise an exception if jwt_secret is not at least 32 characters long.
+DO \$\$ 
+BEGIN
+  IF LENGTH(current_setting('jwt_secret')) < 32 THEN
+    RAISE EXCEPTION 'jwt_secret must be at least 32 characters long';
+  END IF;
+END \$\$;
+
 SET search_path = settings, pg_catalog, public;
 
 INSERT INTO secrets (key, value) VALUES ('jwt_lifetime','3600');
