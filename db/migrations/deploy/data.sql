e437e491a71e632aa58672a59ac3a8ebc37a0b4c
Kyle L. Jensen <kljensen@gmail.com>
2024-03-22 14:50:10 -0400

    Change secret checking

--- db/migrations/deploy/data.sql
+++ db/migrations/deploy/data.sql
@@ -5,14 +5,6 @@
 BEGIN;
 \set jwt_secret `echo $JWT_SECRET`
 
--- Raise an exception if jwt_secret is not at least 32 characters long.
-DO $$ 
-BEGIN
-  IF LENGTH(current_setting('jwt_secret')) < 32 THEN
-    RAISE EXCEPTION 'jwt_secret must be at least 32 characters long';
-  END IF;
-END $$;
-
 SET search_path = settings, pg_catalog, public;
 
 INSERT INTO secrets (key, value) VALUES ('jwt_lifetime','3600');
@@ -20,5 +12,13 @@ INSERT INTO secrets (key, value) VALUES ('auth.default-role','anonymous');
 INSERT INTO secrets (key, value) VALUES ('auth.data-schema','data');
 INSERT INTO secrets (key, value) VALUES ('auth.api-schema','api');
 INSERT INTO secrets (key, value) VALUES ('jwt_secret',:'jwt_secret');
+
+-- Check that jwt_secret is set and greater than 32 characters
+DO $$ 
+BEGIN
+  IF (SELECT length(value) FROM secrets WHERE key = 'jwt_secret') < 32 THEN
+    RAISE EXCEPTION 'jwt_secret must be set and greater than 32 characters';
+  END IF;
+END $$;
 
 COMMIT;
